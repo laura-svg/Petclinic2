5e508fd3f7c97121a2732fd4378881fff012516a
Laura <lauramovsisyan6661@gmail.com>
2025-05-27 17:19:47 +0300

    Add server-port 8080 in yml

--- src/main/java/com/example/petclinic2/Petclinic2Application.java
+++ src/main/java/com/example/petclinic2/Petclinic2Application.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Petclinic2Application {
 
     public static void main(String[] args) {
+try{
         SpringApplication.run(Petclinic2Application.class, args);
+    }
+catch(Throwable e){
+e.printStackTrace();
+}
     }
 
 }
